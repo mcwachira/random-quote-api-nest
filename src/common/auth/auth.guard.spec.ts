2c13a42a64b6e093fbd04025ec2dbdf21a66ce5e
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-05-27 19:07:51 +0300

    added some unit testing for the api

---
+++ src/common/auth/auth.guard.spec.ts
@@ -0,0 +1,55 @@
+import { ExecutionContext } from '@nestjs/common';
+import { AuthGuard } from './auth.guard';
+import { createMock } from '@golevelup/ts-jest';
+describe('AuthGuard', () => {
+  const authGuard = new AuthGuard();
+  it('should be defined', () => {
+    expect(authGuard).toBeDefined();
+  });
+
+  it(`should return true if there is a valid api key`, () => {
+    const context = createMock<ExecutionContext>({
+      switchToHttp: () => ({
+        getRequest: () => ({
+          header: () => `SECRET`,
+          headers: {
+            'x-api-key': `SECRET`,
+          },
+        }),
+      }),
+    });
+
+    const result = authGuard.canActivate(context);
+    expect(result).toBe(true);
+  });
+
+  it(`should return false if no header is passed in `, () => {
+    const context = createMock<ExecutionContext>({
+      switchToHttp: () => ({
+        getRequest: () => ({
+          header: () => undefined,
+          headers: {},
+        }),
+      }),
+    });
+
+    const result = authGuard.canActivate(context);
+    expect(result).toBe(false);
+  });
+
+  it(`should return false if api key is invalid`, () => {
+    const context = createMock<ExecutionContext>({
+      switchToHttp: () => ({
+        getRequest: () => ({
+          header: () => `INVALID`,
+          headers: {
+            'x-api-key': `INVALID`,
+          },
+        }),
+      }),
+    });
+
+    const result = authGuard.canActivate(context);
+    expect(result).toBe(false);
+  });
+});
