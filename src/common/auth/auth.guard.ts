f58020bc799aa19f63dec2b95fe9c70ea4718fe4
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-05-27 19:07:25 +0300

    Added auth to the api

---
+++ src/common/auth/auth.guard.ts
@@ -0,0 +1,19 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const apiKey = request.header(`x-api-key`);
+    console.log(`apiKey:${apiKey}`);
+    if (apiKey === 'SECRET') {
+      console.log(`Guard:passed authentication`);
+      return true;
+    }
+    console.log(`Guard:failed authentication`);
+    return false;
+  }
+}
