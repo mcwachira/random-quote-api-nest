db3368370a1fec05694087a35db4df273c4a9750
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-05-28 19:51:20 +0300

    Built an exception filter

--- src/common/auth/auth.guard.ts
+++ src/common/auth/auth.guard.ts
@@ -1,4 +1,9 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -6,6 +11,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    throw new UnauthorizedException();
     const request = context.switchToHttp().getRequest();
     const apiKey = request.header(`x-api-key`);
     console.log(`apiKey:${apiKey}`);
